f3310f1a575b87514d0d565734dcff641fd0e4c6
MohamedElshewy <abdalluhmohamed@gmail.com>
2025-11-02 18:28:00 +0200

    in progress

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ShapesBar} from './components/shapes-bar/shapes-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ShapesBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
